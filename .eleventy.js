13ac14d45f143d5e630cae421a711d500a4c291b
John Vining <johnvining@gmail.com>
2024-01-15 17:30:40 -0600

    Basic WebC Example

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,23 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
     // Input directory: src
     // Output directory: _site
   
     // The following copies to `_site/img`
     eleventyConfig.addPassthroughCopy("src/style.css");
+
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addPlugin(pluginWebc, {
+      components: "src/_includes/components/**/*.webc",
+    });
+
+
+    return {
+      dir: {
+        input: "src"
+      }
+    }
+  
   };
