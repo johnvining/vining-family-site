08576a5de8b0d31c09035411ea91dd3e65188d0a
John Vining <johnvining@gmail.com>
2024-01-15 20:56:20 -0600

    Example component using javascript

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,20 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
-module.exports = function(eleventyConfig) {
-    // Input directory: src
-    // Output directory: _site
-  
-    // The following copies to `_site/img`
-    eleventyConfig.addPassthroughCopy("src/style.css");
+module.exports = function (eleventyConfig) {
+  // Input directory: src
+  // Output directory: _site
 
-    eleventyConfig.addPlugin(EleventyRenderPlugin);
-    eleventyConfig.addPlugin(pluginWebc, {
-      components: "src/_includes/components/**/*.webc",
-    });
+  eleventyConfig.addPassthroughCopy("src/style.css");
 
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "src/_includes/components/**/*.webc",
+  });
 
-    return {
-      dir: {
-        input: "src"
-      }
-    }
-  
+  return {
+    dir: {
+      input: "src",
+    },
   };
+};
